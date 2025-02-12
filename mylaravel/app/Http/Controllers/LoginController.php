72ac27ba5f2e1bac48a883b96bad971ec7a1e2dd
RyuujiYamaoka <163663120+RyuujiYamaoka@users.noreply.github.com>
2025-02-12 23:05:06 +0700

    ss

---
+++ mylaravel/app/Http/Controllers/LoginController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+ class LoginController extends Controller
+{
+    function index(){
+        return view('login');
+    }
+}
--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -1,11 +1,41 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Mycontroller;
+use App\Http\Controllers\Logincontroller;
+use App\Http\Controllers\Homecontroller;
+use App\Http\Controllers\Registercontroller;
+use App\Http\Controllers\Usercontroller;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
+
+Route::get('/home', [HomeController::class, 'index']);
+
+Route::get('/Mycontroller', [Mycontroller::class, 'myfunction']);
+
+Route::post('/Mycontroller', [Mycontroller::class, 'myfunction']);
+
+Route::post('/register', [RegisterController::class, 'create']);
+
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/user/{id}', [UserController::class, 'edit']);
+Route::put('/user', [UserController::class, 'edit_action']);
+Route::delete('/user', [UserController::class, 'delete']);
+
+Route::get('/404', function(){
+    abort(404);
 });
 
-Route::get('/hello', function () {
-    return "<h1>Hello World!</h>";
+Route::get('/500', function(){
+    abort(500);
+});
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/Hello', function ($val="") {
+    return "<h1>Hello World! </h1>";
 });
